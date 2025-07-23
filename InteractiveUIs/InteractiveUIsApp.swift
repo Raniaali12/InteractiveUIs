5d2cfcf1463c30ae627ab8bc9334de0404f39ac2
Rania Ali <ranianali426@gmail.com>
2025-07-23 14:28:03 -0400

    Initial Commit

---
+++ InteractiveUIs/InteractiveUIsApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveUIsApp.swift
+//  InteractiveUIs
+//
+//  Created by Scholar on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveUIsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
